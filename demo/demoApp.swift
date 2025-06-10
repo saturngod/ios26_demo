6b2ed675f7cf493f4566e115fdfc6d0341c00853
saturngod <saturngod@gmail.com>
2025-06-10 01:37:15 -0500

    Initial Commit

---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by Htain Lin Shwe on 10/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
